36a7ab0e8848e6553f5a0da74d3af0011dd07cc8
jangjimin9766 <jangjimin9766@gmail.com>
2026-03-16 22:17:24 +0900

    chore: re-trigger deployment and add minor comment

--- src/main/java/com/mine/api/MineApplication.java
+++ src/main/java/com/mine/api/MineApplication.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @org.springframework.scheduling.annotation.EnableAsync
+// Initializing the Spring Boot application configuration
 public class MineApplication {
 
     public static void main(String[] args) {
