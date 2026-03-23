8d624569c5c711f498a47da4e8efa393ee99886e
jangjimin9766 <jangjimin9766@gmail.com>
2026-03-23 20:43:30 +0900

    feat(auth): 기본 프로필 이미지를 S3 대신 서버 정적 에셋으로 서빙하도록 변경
    - Figma에서 요청한 회색 기본 실루엣 이미지를 서버의 static/images/ 로 추가
    - AuthService.java: 회원가입 시 지정되는 기본 프로필 URL을 https://mine-server.site/images/default-profile.png 로 변경

--- src/main/java/com/mine/api/service/AuthService.java
+++ src/main/java/com/mine/api/service/AuthService.java
@@ -36,7 +36,7 @@ public class AuthService {
                 .password(passwordEncoder.encode(request.getPassword()))
                 .nickname(request.getNickname())
                 .role(Role.USER)
-                .profileImageUrl("https://mine-moodboard-bucket.s3.ap-southeast-2.amazonaws.com/assets/default-profile.png")
+                .profileImageUrl("https://mine-server.site/images/default-profile.png")
                 .build();
 
         User savedUser = userRepository.save(user);
